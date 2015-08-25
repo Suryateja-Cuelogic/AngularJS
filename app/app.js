ce6f413ea128d34c506e91f11ba2be0708633557
SuryaTeja <suryateja.mahidhara@cuelogic.co.in>
2015-08-25 19:09:03 +0530

    Implemented Authentication Module

--- app/app.js
+++ app/app.js
@@ -1,35 +1,68 @@
 // Declare app level module which depends on views, and components
-var app = angular.module('BlogApp', ['ngRoute']);
+var app = angular.module('BlogApp', ['ngRoute', 'ngStorage']);
 
-app.config(['$routeProvider',function($routeProvider) {
+app.config(['$locationProvider', '$routeProvider', function($location, $routeProvider) {
 	$routeProvider.
 		when('/home', {
 			templateUrl: 'home/index.html',
-			controller: 'HomeController'
+			controller: 'HomeController',
+			access: { requiredLogin: true }
 		}).
 		when('/about-us', {
 			templateUrl: 'aboutus/index.html',
-			controller: 'AboutUsController'
+			controller: 'AboutUsController',
+			access: { requiredLogin: true }
 		}).
 		when('/join-us', {
 			templateUrl: 'careers/index.html',
-			controller: 'CareersController'
+			controller: 'CareersController',
+			access: { requiredLogin: true }
 		}).
 		when('/cuelogic/clients', {
 			templateUrl: 'clients/index.html',
-			controller: 'ClientsController'
+			controller: 'ClientsController',
+			access: { requiredLogin: true }
 		}).
 		when('/terms-and-conditions', {
 			templateUrl: 'toc/index.html',
-			controller: 'TocController'
+			controller: 'TocController',
+			access: { requiredLogin: true }
+		}).
+		when('/login',{
+			templateUrl: 'login/index.html',
+			controller: 'LoginController',
+			access: { requiredLogin: false }
 		}).
 		otherwise({
 			redirectTo: '/home'
 		});
 }]);
 
-app.controller('HomeController',['$scope', function($scope) {
+app.run(function($rootScope, $location, $localStorage) {
+	$rootScope.$on("$routeChangeStart", function(event, nextRoute, currentRoute) {
+        if (nextRoute.access.requiredLogin && !$localStorage.token) {
+            $location.path("/login");
+        }
+    });
+});
+
+app.controller('HomeController',['$scope', '$http', '$localStorage', function($scope, $http, $localStorage) {
+	$scope.token = $localStorage.token;
 	$scope.message = "This is Home Screen";
+	$scope.logout = function() {
+		$http.get('http://localhost:2001/logout', {
+			    headers: {'Authorization': 'Bearer '+$localStorage.token }
+			}).
+			success(function(data) {
+				delete $localStorage.token;
+				window.location = "/app/";  
+			}).	
+			error(function(data) {
+				if (data.statusCode === 403) {
+					$scope.errorMessage = data.message;
+				}
+			});
+	}
 }]).
 controller('AboutUsController',['$scope', function($scope) {
 	$scope.message = "This is AboutUs Screen";
@@ -42,4 +75,22 @@ controller('ClientsController',['$scope', function($scope) {
 }]).
 controller('TocController',['$scope', function($scope) {
 	$scope.message = "This is terms-and-conditions Screen";
+}]).controller('LoginController',['$scope', '$http', '$localStorage', function($scope, $http, $localStorage) {
+	$scope.checkLogin = function() {
+		var loginData = {
+			email : $scope.username,
+			password : $scope.password
+		};
+		
+		$http.post('http://localhost:2001/login',loginData).
+			success(function(data) {
+				$localStorage.token = data.token;
+				window.location = "/app/";  
+			}).	
+			error(function(data) {
+				if (data.statusCode === 403) {
+					$scope.errorMessage = data.message;
+				}
+			});
+	};
 }]);
